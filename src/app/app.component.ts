abd7b45a89f8c709d533657fab16792fc3312c73
jordimall <jordi.mallafre@hotmail.com>
2023-12-07 13:01:02 +0100

    Finalización ta40

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [CommonModule, RouterOutlet, NavbarComponent, HttpClientModule],
 })
 export class AppComponent {
   title = 'router';
